6adeecc02015c5c6ce4d13f3717cd8230f76b93e
aaron blum <blumaa@gmail.com>
2025-10-24 11:26:10 +0200

    fix: storybook docs

--- component-lib/.storybook/YourTheme.ts
+++ component-lib/.storybook/YourTheme.ts
@@ -1,8 +1,10 @@
 import { create } from 'storybook/internal/theming';
+import logo from '../assets/moon-cover.svg';
 
 export default create({
   base: 'light',
   brandTitle: 'Mond Design System',
-  brandUrl: 'https://example.com',
+  brandUrl: 'https://github.com/blumaa/mond-design-system',
+  brandImage: logo,
   brandTarget: '_self',
 });
